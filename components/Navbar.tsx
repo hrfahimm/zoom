3958f4fa6538ebde863f2fa5a90e599e7fcca94e
hrfahimm <fahimtaj10@gmail.com>
2024-04-01 21:24:11 +0600

    nav-45

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,16 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav className=' flex justify-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10 '>
+            <Link href="/" className='flex items-center gap-2 '>
+                <Image alt='woom logo' src="/woom-logo.png" width={35} height={35} className='max-sm:size-10' />
+                <p className="text-[26px]  font-bold text-white max-sm:hidden">WOOM</p>
+            </Link>
+
+        </nav>
     )
 }
 
