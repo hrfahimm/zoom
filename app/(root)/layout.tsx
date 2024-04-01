008d53ce01eb445fb42a1be80179c06e38af3785
hrfahimm <fahimtaj10@gmail.com>
2024-04-01 10:02:40 +0600

    setup

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,9 @@
+import React, { Children, ReactNode } from 'react'
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+    return (
+        <main>{children}</main>
+    )
+}
+
+export default RootLayout
