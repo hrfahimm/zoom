37bbfe8ce3910cb46478759e3a10d54651672e6b
hrfahimm <fahimtaj10@gmail.com>
2024-04-25 10:27:36 +0600

    non

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,22 +1,28 @@
-import { Metadata } from 'next';
-import React from 'react'
-export const metadata: Metadata = {
-    title: "WOOM | HOME",
-    description: " Zoom Clone",
-};
+
+import MeetingTypeList from '@/components/MeetingTypeList';
 const Home = () => {
+    const now = new Date();
+
+
+    const time = now.toLocaleTimeString("en-BD", { hour: '2-digit', minute: '2-digit' });
+
+    const date = (new Intl.DateTimeFormat("en-BD", { dateStyle: "full" })).format(now)
+
+
     return (
         <section className='flex size-full flex-col gap-10 text-white'>
             <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
                 <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11 ">
                     <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal'> Upcomeing Meeting at : <span className='font-bold'>12:30 PM</span></h2>
                     <div className="flex flex-col gap-2">
-                        <h1 className='text-4xl font-extrabold lg:text-7xl '  > 11:30 AM</h1>
-                        <p className='text-lg font-medium text-[#c9ddff] lg:text-2xl'>Wednesdat, April 3, 2024</p>
+                        <h1 className='text-4xl font-extrabold lg:text-7xl '  > {time} </h1>
+                        <p className='text-lg font-medium text-[#c9ddff] lg:text-2xl'>{date} </p>
+
                     </div>
 
                 </div>
             </div>
+            <MeetingTypeList />
         </section>
     )
 }
---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+
+
+const MeetingTypeList = () => {
+    return (
+        <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4 '>
+
+            <div className=" bg-[#FF742E] px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer" >
+
+                <div className=" bg-[#FF742E] px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer" >
+
+
+                </div>
+
+            </div>
+
+        </section>
+    )
+}
+
+export default MeetingTypeList
