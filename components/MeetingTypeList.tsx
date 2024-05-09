1ee38b571c0a76c0f05a74cd7cde8d2bc5638c9e
hrfahimm <fahimtaj10@gmail.com>
2024-05-09 21:55:45 +0600

    secondtime

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import Image from "next/image"
+
 
 
 const MeetingTypeList = () => {
@@ -8,8 +10,14 @@ const MeetingTypeList = () => {
 
             <div className=" bg-[#FF742E] px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer" >
 
-                <div className=" bg-[#FF742E] px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer" >
+                <div className=" bg-[#FF742E] px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer"
+                    onClick={() => { }}>
 
+                    <div className="flex-center glassmorphism size-12 rounded-[10px]">\
+
+                        <Image src='/icons/add-meeting.svg' alt="metting-icon" />
+
+                    </div>
 
                 </div>
 
